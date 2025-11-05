cb6271fdc0c4d48d5cca1cfbc3f04be21de8eaab
Paulo E. Heymann <pehlpgamer@gmail.com>
2025-11-05 15:06:59 +0800

    Add project files.

---
+++ HCI2_web_api/Controllers/TrashbinController.cs
@@ -0,0 +1,65 @@
+﻿using HCI2_web_api.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HCI2_web_api.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrashbinController : ControllerBase
+    {
+
+
+        private List<Trashbin> trashbins = new List<Trashbin>
+        {
+            //sample data
+            new Trashbin
+            {
+                TrashbinId = 321,
+                BodyColor = "black",
+                StorageSizeInLiters = 5.5,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            },
+            new Trashbin
+            {
+                TrashbinId = 124,
+                BodyColor = "blue",
+                StorageSizeInLiters = 20,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            }
+        };
+
+
+        [HttpGet]
+        public ActionResult<List<Trashbin>> GetTrashbins()
+        {
+            if (trashbins == null) return NoContent();
+            else return Ok(trashbins);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Trashbin> GetTrashbinById(int id)
+        {
+
+            var trashbin = trashbins.FirstOrDefault(x => x.TrashbinId == id);
+            if (trashbin == null) return NotFound();
+            return Ok(trashbin);
+            
+        }
+
+        [HttpPost]
+        public ActionResult<Trashbin> AddTrashBin(Trashbin newTrashbin)
+        {
+            if (newTrashbin == null) return BadRequest();
+            trashbins.Add(newTrashbin);
+            return CreatedAtAction(nameof(GetTrashbinById), new {id = newTrashbin.TrashbinId}, newTrashbin);
+        }
+
+        
+
+    }
+}
---
+++ HCI2_web_api/models/Trashbin.cs
@@ -0,0 +1,24 @@
+﻿namespace HCI2_web_api.models
+{
+    public enum binIntegrity
+    {
+        NeedsReplacement,
+        Low,
+        High
+    }
+    public class Trashbin
+    {
+        public int TrashbinId { get; set; }
+        public binIntegrity BinIntegrity { get; set; } 
+
+        public string BodyColor { get; set; } 
+        public double StorageSizeInLiters { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        
+    }
+}
